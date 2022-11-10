92f7529a35820a72dbe6db7d711a503921489b0f
Ruben Chirino <rubenchch21@gmail.com>
2022-11-09 21:24:22 -0300

    [Fix] Setting the correct structure to the gallery view file

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,8 +1,7 @@
-
-
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -34,3 +33,6 @@
 </div>
 
 @endsection
+
+
+
