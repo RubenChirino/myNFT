d4e1778078d3a908e755dbbdce2ba262c8323255
Ruben Chirino <rubenchch21@gmail.com>
2022-11-12 12:59:32 -0300

    [Fix] Correcting the structure for the home & gallery ppages

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,38 +1,52 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+@endsection --}}
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+
+
+<x-app-layout>
+
+    <!-- Scripts -->
+    {{-- @vite(['resources/css/views/auth/dashboard/nfts/add.css']) --}}
+
+  <div class="py-12">
+
+    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ __('Dashboard') }}</div>
+
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+
+                            {{ __('You are logged in!') }}
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
+
+            @foreach( $nfts as $nft )
+            <p>
+                <strong>{{ $nft->id }}</strong>
+
+                <br>
+                <span>{{ $nft->title }}</span>
+            </p>
+            @endforeach
+
         </div>
+
     </div>
 
-    @foreach( $nfts as $nft )
-    <p>
-        <strong>{{ $nft->id }}</strong>
+  </div>
 
-        <br>
-        <span>{{ $nft->title }}</span>
-    </p>
-    @endforeach
-
-</div>
-
-@endsection
-
-
-
+</x-app-layout>
