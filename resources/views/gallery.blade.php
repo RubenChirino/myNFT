2a99091c125e251bb4763ea3f688b898cf2c15e7
Ruben Chirino <rubenchch21@gmail.com>
2022-11-13 16:38:03 -0300

    [Feature] Setting the html & styles for the gallery page

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -9,42 +9,32 @@
 <x-app-layout>
 
     <!-- Scripts -->
-    {{-- @vite(['resources/css/views/auth/dashboard/nfts/add.css']) --}}
+    @vite(['resources/css/views/gallery.css'])
 
-  <div class="py-12">
+  <div class="gallery-page py-9">
 
-    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+    <h1 class="title">Gallery</h1>
 
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="nfts-container">
 
-                        <div class="card-body">
-                            @if (session('status'))
-                                <div class="alert alert-success" role="alert">
-                                    {{ session('status') }}
-                                </div>
-                            @endif
-
-                            {{ __('You are logged in!') }}
-                        </div>
+        @foreach( $nfts as $nft )
+            {{-- <x-nft-card :title={{ $nft->title }} :price={{ $nft->price }} /> --}}
+            <div class="nft-card max-w-sm rounded-md overflow-hidden shadow-lg cursor-pointer">
+                <img class="card-img-top" src="{{ asset('/img/nfts/orochi-silver.png') }}" alt="...">
+                <div class="px-3 py-2">
+                    <h5 class="card-title font-bold truncate">{{ $nft->title }}</h5>
+                    <div>
+                        <span class="font-bold">Price:</span>
+                        <p class="font-bold">{{ $nft->price }} ARS</p>
                     </div>
                 </div>
             </div>
+        @endforeach
 
-            @foreach( $nfts as $nft )
-            <p>
-                <strong>{{ $nft->id }}</strong>
+    </div>
 
-                <br>
-                <span>{{ $nft->title }}</span>
-            </p>
-            @endforeach
-
-        </div>
-
+    <div class="pagination-container">
+        {{ $nfts->links() }}
     </div>
 
   </div>
