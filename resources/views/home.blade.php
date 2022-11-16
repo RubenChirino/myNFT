68afab72eff8d93ec27b5f26c9fa43d656c89a1a
francoouro <francoouro844@gmail.com>
2022-11-16 01:57:36 -0300

    Modifield Home and Creating Styles for the home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-
+    <link rel="stylesheet" href="/css/home.css">
     <!-- Scripts -->
     {{-- @vite(['resources/css/views/auth/dashboard/nfts/add.css']) --}}
 
@@ -7,10 +7,47 @@
 
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
 
-        CONTENT
+        <div class= "mb-5 text-5xl text-black font-extrabold text-center">Discover and collect extraordinary NFTs</div>
 
+        <div class="mb-10 text-3xl text-center">myNFT is the marketplace to buy NFTs in an easier way</div>
+
+        <div id="text-box">
+            <img  id="img-home"src="/img/aliens-collection.jpeg" alt="" class="img-fluid">
+            <div id="text_above" class="text-center">Alien Collection Coming soon</div>
+        </div>
+
+        <div>
+            <img src="/img/sponsors/mark-zuckerberg.png" alt="" class="max-w-full h-auto round text-center text-align-center">
+            <div class="mb-4 text-5xl text-center">"MyNFT is better than any other platform we've played with, and we've used them all"</div>
+            <div class="mb-1 text-2xl text-center font-extrabold">Mark Zuckerberg, CEO of Meta</div>
+        </div>
+
+        <div class="grid grid-cols-2 gap-x-10 flex items-center">
+
+            <div class="mb-4 m-10 ml-auto">
+                <img src="/img/sponsors/blockchain-capital.png" alt="">
+            </div>
+
+            <div class="mb-4 m-10 mr-auto">
+                <img src="/img/sponsors/meta.png" alt="">
+            </div>
+
+            <div class="mb-4 m-10 ml-auto">
+                <img src="/img/sponsors/yCombinator.png" alt="">
+            </div>
+
+            <div class="mb-4 m-10 mr-auto">
+                <img src="/img/sponsors/paradigm-capital.png" alt="">
+            </div>
+        </div>
+
+        <div class="grid grid-cols-1 gap-4 flex items-center">
+
+            <div class="mr-auto ml-auto w-68">
+                <img src="/img/sponsors/binance.png" alt="">
+            </div>
+
+        </div>
     </div>
-
   </div>
-
 </x-app-layout>
