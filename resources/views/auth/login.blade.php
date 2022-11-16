e426b0abac899bd3c15a50d7bddb5942fce44c98
Ruben Chirino <rubenchch21@gmail.com>
2022-11-15 21:56:31 -0300

    [Feature] Creating the login form component and using it

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,39 +31,8 @@
             <h1 class="login-title">Sign in</h1>
 
             <form method="POST" action="{{ route('login') }}">
-                @csrf
-
-              <!-- Email -->
-              <div class="form-group mb-8">
-
-                <label for="email">Email</label>
-
-                <input type="email"
-                        name="email" id="email"
-                        :value="__('Email')" class="form-control" required autofocus
-                        placeholder="Enter your email address...">
-
-                <x-input-error :messages="$errors->get('email')" class="mt-2" />
-
-              </div>
-
-              <!-- Password -->
-              <div class="form-group mb-10">
-
-                <label for="password">Password</label>
-
-                <input type="password"
-                        name="password"
-                        id="password" class="form-control"
-                        :value="__('Password')" required autocomplete="current-password"
-                        placeholder="Enter your password...">
-
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
-
-              </div>
-
-              <input class="btn btn-block bg-blue-600 hover:bg-blue-700 login-btn" name="login" id="login" type="submit" value="{{ __('Sign In') }}">
-
+                @include('auth._form')
+                <input class="btn btn-block bg-blue-600 hover:bg-blue-700 login-btn" name="login" id="login" type="submit" value="{{ __('Sign In') }}">
             </form>
 
             <div class="w-auto p-1 ml-auto">
