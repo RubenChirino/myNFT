ab2b322edfaa62b8117ba77e536248938421033b
Ruben Chirino <rubenchch21@gmail.com>
2022-11-16 22:47:37 -0300

    [Feature] Adding the initial structure for the Nft admin page

--- resources/views/admin/dashboard/nfts/index.blade.php
+++ resources/views/admin/dashboard/nfts/index.blade.php
@@ -1,44 +1,61 @@
 <x-app-layout>
-    <x-slot name="header">
+    {{-- <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Nfts') }}
         </h2>
 
         <a href="{{ route('nfts.create') }}">Crear</a>
-    </x-slot>
+    </x-slot> --}}
 
-    <div class="py-12 ruben">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+    <!-- Styles -->
+    <x-bootstrap-css />
+    @vite(['resources/css/views/dashboard/nfts/index.css'])
 
-                    <table class="mb-4">
-                        @foreach ($nfts as $nft)
-                        <tr class="border-b border-gray-200 text-sm">
-                            <td class="px-6 py-4">{{ $nft->name }}</td>
-                            <td class="px-6 py-4">
-                                <a href="{{ route('nfts.edit', $nft) }}" class="text-indigo-600">Editar</a>
-                            </td>
-                            <td class="px-6 py-4">
-                                <form action="{{ route('nfts.destroy', $nft) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <input
-                                    class="bg-gray-800 text-white rounded px-4 py-2"
-                                    type="submit"
-                                    value="Eliminar"
-                                    onclick="return confirm('Do you want to eliminate it?')">
-                                </form>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </table>
+    <div class="admin-nfts-page py-12">
 
-                    {{ $nfts->links() }}
+        <h1 class="title">Admin Dashboard</h1>
 
-                </div>
+        <div class="table-container bg-white mx-auto sm:px-4 lg:px-7 overflow-hidden shadow rounded-md">
+            <table class="table table-striped-columns">
+                <thead>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Product</th>
+                      <th scope="col">Price</th>
+                      <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach ($nfts as $nft)
+                <tr class="border-b border-gray-200 text-sm">
+                    <td id="nft-id_column" class="px-6 py-2">{{ $nft->id }}</td>
+                    <td id="nft-name_column" class="px-6 py-2">
+                        {{ substr($nft->name, 0, 15) }}
+                    </td>
+                    <td id="nft-price_column" class="px-6 py-2">{{ $nft->price }}</td>
+                    <td id="nft-actions_column" class="px-6 py-2">
+                        <a href="{{ route('nfts.edit', $nft) }}" class="text-indigo-600">Editar</a>
+
+                        <form action="{{ route('nfts.destroy', $nft) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <input
+                            class="bg-gray-800 text-white rounded px-4 py-2"
+                            type="submit"
+                            value="Eliminar"
+                            onclick="return confirm('Do you want to eliminate it?')">
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+                </tbody>
+            </table>
+
+            <div class="pagination-container">
+                {{ $nfts->links() }}
             </div>
         </div>
+
     </div>
 
 </x-app-layout>
@@ -57,11 +74,11 @@
                     <table class="mb-4">
                         @foreach ($nfts as $nft)
                         <tr class="border-b border-gray-200 text-sm">
-                            <td class="px-6 py-4">{{ $nft->name }}</td>
-                            <td class="px-6 py-4">
+                            <td class="px-6 py-2">{{ $nft->name }}</td>
+                            <td class="px-6 py-2">
                                 <a href="" class="text-indigo-600">Editar</a>
                             </td>
-                            <td class="px-6 py-4">Eliminar</td>
+                            <td class="px-6 py-2">Eliminar</td>
                         </tr>
                         @endforeach
                     </table>
