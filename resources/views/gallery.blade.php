1e25c1807b948b329b0b3dd0b1b97847a92623d3
Ruben Chirino <rubenchch21@gmail.com>
2022-11-19 14:38:41 -0300

    [Feature] Adding some details to the gallary page

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -14,10 +14,15 @@
             <div class="nft-card max-w-sm rounded-md overflow-hidden shadow-lg cursor-pointer">
                 <img class="card-img-top" src="{{ asset('/img/nfts/orochi-silver.png') }}" alt="...">
                 <div class="px-3 py-2">
-                    <h5 class="card-title font-bold truncate">{{ $nft->name }}</h5>
-                    <div>
+                    <div id="row-1">
+                        <h5 class="card-title font-bold truncate">{{ $nft->name }}</h5>
+                        <button class="card-add-to-cart-btn">
+                            <img class="bg-blue-600 p-1 rounded" src="{{ asset('/img/icons/cart-white.png') }}" alt="">
+                        </button>
+                    </div>
+                    <div id="row-2">
                         <span class="font-bold">Price:</span>
-                        <p class="font-bold">{{ $nft->price }} ARS</p>
+                        <p class="font-bold">{{ number_format($nft->price, 2, ",", ".") }} ARS</p>
                     </div>
                 </div>
             </div>
