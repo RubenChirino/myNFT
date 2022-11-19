0be86a90a4550a99e58437ca2b8a9b796c87114e
Ruben Chirino <rubenchch21@gmail.com>
2022-11-19 16:05:23 -0300

    [Feature] Adding the 'Add to cart' funct in the gallery page

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -11,12 +11,12 @@
 
         @foreach( $nfts as $nft )
             {{-- <x-nft-card :name={{ $nft->name }} :price={{ $nft->price }} /> --}}
-            <div class="nft-card max-w-sm rounded-md overflow-hidden shadow-lg cursor-pointer">
+            <div id="nft-{{ $nft->id }}-card" class="nft-card max-w-sm rounded-md overflow-hidden shadow-lg cursor-pointer">
                 <img class="card-img-top" src="{{ asset('/img/nfts/orochi-silver.png') }}" alt="...">
                 <div class="px-3 py-2">
                     <div id="row-1">
                         <h5 class="card-title font-bold truncate">{{ $nft->name }}</h5>
-                        <button class="card-add-to-cart-btn">
+                        <button id="nft-{{ $nft->id }}-btn" onclick="addToCart({{ $nft }})" class="card-add-to-cart-btn">
                             <img class="bg-blue-600 p-1 rounded" src="{{ asset('/img/icons/cart-white.png') }}" alt="">
                         </button>
                     </div>
@@ -27,7 +27,6 @@
                 </div>
             </div>
         @endforeach
-
     </div>
 
     <div class="pagination-container">
@@ -35,5 +34,55 @@
     </div>
 
   </div>
+
+  <script>
+    const cart = JSON.parse(window.localStorage.getItem("cart") ?? {});
+
+    function addToCart(item) {
+        if (!item) {
+            console.error("Not Item received");
+            return;
+        }
+
+        // console.log("DATA =>", {
+        //     cart,
+        //     item: cart[item.id],
+        // });
+
+
+        // Verify
+        if (!cart[item.id]) {
+            cart[item.id] = {
+                name: item.name,
+                price: item.price,
+                category: item.category,
+            }
+            try {
+                window.localStorage.setItem("cart", JSON.stringify(cart));
+                removeAddToCartBtn(item.id);
+            } catch (error) {
+                console.error("An error has ocurred to add the new item to the cart", error);
+            }
+        } else {
+            console.warn("The item is already in the cart", cart[item.id]);
+        }
+
+        return;
+    }
+
+    function removeAddToCartBtn(id) {
+        const btn = document.querySelector(`#nft-${id}-btn`);
+        // console.log("btn =>", btn);
+        // btn.classList.add("in-cart");
+        btn.remove();
+    }
+
+    // Product added to the cart
+    for (const [key, value] of Object.entries(cart)) {
+        // console.log(`${key}: ${value}`);
+        removeAddToCartBtn(key);
+    }
+
+  </script>
 
 </x-app-layout>
