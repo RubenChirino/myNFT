47509370ef6fd0168e6a657d44770f312d0824bb
Ruben Chirino <rubenchch21@gmail.com>
2022-11-18 22:25:28 -0300

    [Feature] Adding some details to the home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,9 @@
 <x-app-layout>
-    
-    <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
 
-    <!-- Scripts -->
-    {{-- @vite(['resources/css/views/auth/dashboard/nfts/add.css']) --}}
-   
-
-  <div class="py-12">
-
+  <!-- Styles -->
   @vite(['resources/css/views/home.css'])
-
-    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+  {{-- max-w-6xl mx-auto sm:px-6 lg:px-8 --}}
+    <div class="home-page">
 
         <div class= "mb-5 text-5xl text-black font-extrabold text-center">Discover and collect extraordinary NFTs</div>
 
@@ -21,38 +14,48 @@
             <div id="text_above" class="text-center">Alien Collection Coming soon</div>
         </div>
 
-        <div>
+        <div class="trust-us">
             <img src="/img/sponsors/mark-zuckerberg.png" alt="" class="max-w-full h-auto round text-center text-align-center">
-            <div class="mb-4 text-5xl text-center">"MyNFT is better than any other platform we've played with, and we've used them all"</div>
-            <div class="mb-1 text-2xl text-center font-extrabold">Mark Zuckerberg, CEO of Meta</div>
+            <div class="description text-center">"MyNFT is better than any other platform we've played with, and we've used them all"</div>
+            <div class="author mb-1 text-center font-extrabold">Mark Zuckerberg, CEO of Meta</div>
         </div>
 
         <div class="grid grid-cols-2 gap-x-10 flex items-center">
 
             <div class="mb-4 m-10 ml-auto">
-                <img src="/img/sponsors/blockchain-capital.png" alt="">
+                <a href="https://blockchain.capital" target="_blank">
+                    <img src="/img/sponsors/blockchain-capital.png" alt="">
+                </a>
             </div>
 
             <div class="mb-4 m-10 mr-auto">
-                <img src="/img/sponsors/meta.png" alt="">
+                <a href="https://www.facebook.com" target="_blank">
+                    <img src="/img/sponsors/meta.png" alt="">
+                </a>
             </div>
 
             <div class="mb-4 m-10 ml-auto">
-                <img src="/img/sponsors/yCombinator.png" alt="">
+                <a href="https://www.ycombinator.com" target="_blank">
+                    <img src="/img/sponsors/yCombinator.png" alt="">
+                </a>
             </div>
 
             <div class="mb-4 m-10 mr-auto">
-                <img src="/img/sponsors/paradigm-capital.png" alt="">
+                <a href="https://www.paradigmcap.com" target="_blank">
+                    <img src="/img/sponsors/paradigm-capital.png" alt="">
+                </a>
             </div>
         </div>
 
         <div class="grid grid-cols-1 gap-4 flex items-center">
 
             <div class="mr-auto ml-auto w-68">
-                <img src="/img/sponsors/binance.png" alt="">
+                <a href="https://www.binance.com" target="_blank">
+                    <img id="binance-icon" src="/img/sponsors/binance.png" alt="">
+                </a>
             </div>
 
         </div>
     </div>
-  </div>
+
 </x-app-layout>
