3fb1ecacec0bc54e7c6f439313b09563acbd2155
Ruben Chirino <rubenchch21@gmail.com>
2022-11-23 23:21:58 -0300

    [Feature] Adding the Shopping table model

---
+++ app/Models/Shopping.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shopping extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'nft_id',
+    ];
+}
